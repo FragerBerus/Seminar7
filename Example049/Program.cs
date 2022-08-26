24f42ba05c4777733f3424f8d9b7e732a25674ed
Berus Frager <gitmailru@yandex.ru>
2022-08-26 21:48:55 +0300

    Коммит файлов папки проекта Example049

---
+++ Example049/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 49:
+// Задайте двумерный массив. Найдите элементы, у которых оба
+// индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+Console.Clear();
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] GetArray(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"Введите элемент [{i}, {j}]: ");
+            matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    return matrix;
+}
+int[,] result = GetArray(rows, columns);
+void PrintArray(int[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int m = 0; m < inputMatrix.GetLength(1); m++)
+        {
+            Console.Write(inputMatrix[i, m] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Начальная матрица:");
+PrintArray(result);
+for (int k = 0; k < rows; k++)
+{
+    for (int i = 0; i < columns; i++)
+    {
+        if (k == i)
+        {
+            result[k, i] = result[k, i] * result[k, i];
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Измененная матрица:");
+PrintArray(result);

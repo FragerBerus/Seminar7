811eb74226e1a3e2aeb7852b14a9434cdccefa46
Berus Frager <gitmailru@yandex.ru>
2022-08-26 21:59:40 +0300

    Коммит файлов папки проекта Example051

---
+++ Example051/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 51:
+// Найти сумму элементов главной диагонали
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+Console.Clear();
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int[,] GetArray(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(11);
+        }
+    }
+    return matrix;
+}
+int[,] result = GetArray(rows, columns);
+void PrintArray(int[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int m = 0; m < inputMatrix.GetLength(1); m++)
+        {
+            Console.Write(inputMatrix[i, m] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Матрица:");
+PrintArray(result);
+for (int k = 0; k < rows; k++)
+{
+    for (int i = 0; i < columns; i++)
+    {
+        if (k == i)
+        {
+            sum = sum + result[k, i];
+        }
+    }
+}
+Console.WriteLine($"Сумма элементов главной диагонали: {sum}");
